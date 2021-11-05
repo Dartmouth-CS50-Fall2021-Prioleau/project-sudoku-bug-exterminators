bfac65bad45b79046898b61a4153725688ffe80d
Alan W. Sun <asun17904@gmail.com>
2021-11-05 13:41:18 -0400

    added sudoku board and boxes data structure and functional prototypes for other modules.

---
+++ display.h
@@ -0,0 +1,14 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+/* Alan Sun, Arden Guo
+ * CS50, Fall 2021, Sudoku Solver Project
+ * Display Sudoku (display.h)
+ *
+ * Function prototypes for module that diplays a given sudoku board.
+ */
+
+#include "board.h"
+
+void display(sudoku_t *board);
+
+#endif 
