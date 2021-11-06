28c8f9ba44533164b6cf2350b5ae2ab0fc61bf2b
Alan W. Sun <asun17904@gmail.com>
2021-11-06 13:34:01 -0400

    constructed header files for sudoku solver

---
+++ solve.h
@@ -0,0 +1,14 @@
+#ifndef SOLVE_H
+#define SOLVE_H
+/* Alan Sun, Arden Guo
+ * CS50, Fall 2021, Sudoku Project
+ * Header for sudoku solver (solve.h)
+ *
+ * This file contains functions that solves a given sudoku board.
+ */
+
+#include<stdbool.h>
+
+bool solve_board(sudoku_t *board);
+
+#endif
