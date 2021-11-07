c994aa8c5ed6221daec0cf4841efaa42b626e0e2
Jinxu Guo <ardenguo@Jinxus-MacBook-Air.local>
2021-11-06 21:42:43 -0400

    delete >>>>HEAD

--- read.c
+++ read.c
@@ -217,7 +217,6 @@ sudoku_t *sudoku_ini(bool difficult, sudoku_t* sudoku)
     return sudoku;
 }
 
-<<<<<<< HEAD
 /* (description): Calloc for an array of size passed in
  *
  * (inputs): dim - represent the size of the array
